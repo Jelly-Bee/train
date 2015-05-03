a430c7db5fe7938a397e4af387a1a5f128fc7d69
keyrinrin <z452202486@gmail.com>
2015-05-04 00:35:20 +0800

    005

---
+++ 004/r.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+def count(filepath):
+    f = open(filepath, 'rb')
+    s = f.read()
+    words = re.findall(r'[a-zA-Z0-9\-]+', s)
+    return len(words)
+
+if __name__ == '__main__':
+    num = count('git.txt')
+    print num
+
+ #reference site http://blog.sina.com.cn/s/blog_a15aa56901017liq.html
+ #http://www.cnblogs.com/sevenyuan/archive/2010/12/06/1898075.html
+ #http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386832260566c26442c671fa489ebc6fe85badda25cd000
+ #http://www.cnblogs.com/huxi/archive/2010/07/04/1771073.html
---
+++ 005/5.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# referecne site PIL  http://zhan.renren.com/hopeinmind?gid=3602888498036325669&checked=true
+# glob  http://blog.csdn.net/csapr1987/article/details/7469769
+# os module http://www.cnblogs.com/BeginMan/p/3327291.html
+
+
+from PIL import Image
+import glob
+
+def get_list():
+	return glob.glob(r'./*.jpg')
+
+def image_process(image_name):
+	im = Image.open(image_name)
+	(x_r,y_r) = im.size
+#按比例调整大小
+	if x_r > y_r:
+		x_s = 640
+		y_s = y_r * x_s / x_r
+	else:
+		y_s = 1136
+		x_s = x_r * y_s / y_r
+	im2 = im.resize((x_s,y_s))
+	im2.save(image_name+'_new.jpg','jpeg')
+
+def change():
+	img_list = get_list()
+	for li in img_list:
+		image_process(li)
+
+
+if __name__ == '__main__':
+			change()
